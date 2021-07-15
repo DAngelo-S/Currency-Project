b83d2ec4e6092c54bcff1d6167f15ccd7891f582
Débora <debora.d.r.a@usp.br>
2021-07-15 06:21:19 -0300

    add request errors

--- erro.py
+++ erro.py
@@ -1,14 +1,36 @@
-def add_log(where, expected, received):
+from datetime import datetime
+
+class Error(Exception):
+    pass
+
+class RequestError(Error):
+    def __init__(self, status):
+        message = error_msg("Request Exchange API - catch_exchange", 200, status)
+        add_log(message)
+        self.message = message
+
+class TimeRequestError(Error):
+    def __init__(self, tries):
+        message = error_msg("Request Exchange API - catch_exchange", "", "Exceeded the limit of tries ({})".format(tries))
+        add_log(message)
+        self.message = message
+
+def UnknownError(local, ex):
+        message = error_msg(local, "", ex)
+        add_log(message)
+        return message
+
+def error_msg(where, expected, received):
     where = str(where)
     expected = str(expected)
     received = str(received)
     
     date = datetime.now()
     date = date.strftime("%d/%m/%Y %H:%M:%S")
-    erro_msg = date + " Error local: " + where + "\n\tExpected: " + expected + "\n\tReceived: " + received + "\n"
-    
-    with open ('log_file', 'a') as log:
-        log.write(erro_msg)
-    
-    print("Erro! Verifique o arquivo de log!\n")
-    exit(0)
+    msg = date + " Error local: " + where + "\n\tExpected: " + expected + "\n\tReceived: " + received + "\n"
+
+    return msg
+
+def add_log(msg):
+    with open('log_file', 'a') as log:
+        log.write(msg)
