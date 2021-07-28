75ea40d440b3ade376f2e59164dadd3c1da2e54e
Débora <debora.d.r.a@usp.br>
2021-07-28 08:21:04 -0300

    modularized draw chart

---
+++ static/draw_chart.js
@@ -0,0 +1,54 @@
+// Load the Visualization API and the corechart package
+google.charts.load('current', {'packages':['corechart']});
+
+// Set a callback for when the API is loaded
+google.charts.setOnLoadCallback(drawChart);
+
+// This is the callback function which actually draws the chart
+function drawChart(){
+
+	dataTable = new google.visualization.DataTable(data);
+	options.hAxis.ticks = dataTable.getDistinctValues(0);
+
+	view = new google.visualization.DataView(dataTable);
+	
+	options['series'] = Series();
+
+	chart = new google.visualization.LineChart(document.getElementById(containerId));
+
+	chart.draw(view, options);
+
+	//google.visualization.drawChart({
+	//	"containerId": containerId,
+	//	"chartType": chartType,
+	//	"options": options,
+	//	"dataTable": data,
+	//	"view": view
+	//})
+}
+
+function Series() {
+    var arr = view.getViewColumns();
+    s = {}
+    for (let i = 1; i < arr.length; i++) {
+        var country = dataTable.getColumnId(arr[i]);
+        if (america.includes(country))
+            s[i-1] = { 'color': 'green'};
+        else if (asia.includes(country) && europe.includes(country))
+            s[i-1] = { 'color': 'purple'};
+        else if (asia.includes(country) && africa.includes(country))
+            s[i-1] = { 'color': 'orange'};
+        else if (asia.includes(country))
+            s[i-1] = { 'color': 'red'};
+        else if (europe.includes(country))
+            s[i-1] = { 'color': 'blue'};
+        else if (oceania.includes(country))
+            s[i-1] = { 'color': 'pink'};
+        else if (africa.includes(country))
+            s[i-1] = { 'color': 'yellow'};
+        else
+            s[i-1] = { 'color': 'black'};
+    }
+
+    return s;
+}
