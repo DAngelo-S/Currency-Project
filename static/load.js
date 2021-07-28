6e14cf00633261e8052820927899886a806195ce
Débora <debora.d.r.a@usp.br>
2021-07-28 07:28:16 -0300

    modularized loading script + modified name of chart.html

---
+++ static/load.js
@@ -0,0 +1,41 @@
+function load_checkboxes() {
+    for (let i = 0; i < america.length; i++)
+        createCheckbox(america[i], 'america');
+    document.getElementById('america').checked = true;
+
+    for (let i = 0; i < asia.length; i++)
+        createCheckbox(asia[i], 'asia');
+    document.getElementById('asia').checked = true;
+
+    for (let i = 0; i < africa.length; i++)
+        createCheckbox(africa[i], 'africa');
+    document.getElementById('africa').checked = true;
+
+    for (let i = 0; i < oceania.length; i++)
+        createCheckbox(oceania[i], 'oceania');
+    document.getElementById('oceania').checked = true;
+
+    for (let i = 0; i < europe.length; i++)
+        createCheckbox(europe[i], 'europe');
+    document.getElementById('europe').checked = true;
+}
+
+function load_options() {
+    myDiv = document.getElementById('period');
+    for (var p in period) {
+        var opt = document.createElement('option');
+        opt.value=p;
+        opt.text=p + ' days';
+        opt.selected='selected';
+        myDiv.appendChild(opt);
+    }
+}
+
+function resize_chart() {
+    chart.draw(view, options);
+}
+
+function loadAll() {
+    load_checkboxes()
+    load_options()
+}
